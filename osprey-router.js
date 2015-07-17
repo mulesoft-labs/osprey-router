e7da706d1aa7816d3da24a08374ae0cc7f851382
Blake Embrey <hello@blakeembrey.com>
2015-07-17 10:43:34 +1000

    Use standard syntax

--- osprey-router.js
+++ osprey-router.js
@@ -1,13 +1,13 @@
-var Engine = require('router/engine');
-var methods = require('methods');
-var flatten = require('array-flatten');
-var ramlPath = require('raml-path-match');
-var slice = Array.prototype.slice;
+var Engine = require('router/engine')
+var methods = require('methods')
+var flatten = require('array-flatten')
+var ramlPath = require('raml-path-match')
+var slice = Array.prototype.slice
 
 /**
  * Expose `router`.
  */
-module.exports = router;
+module.exports = router
 
 /**
  * Initialize router instance.
@@ -16,71 +16,71 @@ module.exports = router;
  * @return {Function}
  */
 function router (options) {
-  return new Router(options);
+  return new Router(options)
 }
 
 /**
  * Construct a router instance.
  */
 function Router (options) {
-  return Engine.call(this, options);
+  return Engine.call(this, options)
 }
 
 /**
  * Inherits from the router engine.
  */
-Router.prototype = Object.create(Engine.prototype);
+Router.prototype = Object.create(Engine.prototype)
 
 /**
  * Create a `raml-path-match` compatible `.use`.
  */
 Router.prototype.use = function use () {
-  var offset = 0;
-  var path = '/';
-  var schema = {};
+  var offset = 0
+  var path = '/'
+  var schema = {}
 
   if (typeof arguments[0] !== 'function') {
-    path = arguments[0];
-    offset = 1;
+    path = arguments[0]
+    offset = 1
 
     if (typeof arguments[1] !== 'function') {
-      schema = arguments[1];
-      offset = 2;
+      schema = arguments[1]
+      offset = 2
     }
   }
 
-  var callbacks = flatten(slice.call(arguments, offset));
+  var callbacks = flatten(slice.call(arguments, offset))
 
   var match = ramlPath(path, schema, {
     sensitive: this.caseSensitive,
     strict: this.strict,
     end: false
-  });
+  })
 
-  return Engine.prototype.use.call(this, path, match, callbacks);
-};
+  return Engine.prototype.use.call(this, path, match, callbacks)
+}
 
 /**
  * Create a `raml-path-match` compatible route.
  */
-Router.prototype.route = function route(path, schema) {
+Router.prototype.route = function route (path, schema) {
   var match = ramlPath(path, schema, {
     sensitive: this.caseSensitive,
     strict: this.strict,
     end: true
-  });
+  })
 
-  return Engine.prototype.route.call(this, path, match);
-};
+  return Engine.prototype.route.call(this, path, match)
+}
 
 // create Router#VERB functions
-methods.concat('all').forEach(function (method){
+methods.concat('all').forEach(function (method) {
   Router.prototype[method] = function (path, schema) {
-    var hasSchema = typeof schema !== 'function';
-    var route = this.route(path, hasSchema ? schema : null);
+    var hasSchema = typeof schema !== 'function'
+    var route = this.route(path, hasSchema ? schema : null)
 
-    route[method].apply(route, slice.call(arguments, hasSchema ? 2 : 1));
+    route[method].apply(route, slice.call(arguments, hasSchema ? 2 : 1))
 
-    return this;
-  };
-});
+    return this
+  }
+})
--- test.js
+++ test.js
@@ -1,175 +1,177 @@
-require('es6-promise').polyfill();
+/* global describe, it */
 
-var methods = require('methods');
-var expect = require('chai').expect;
-var popsicle = require('popsicle');
-var server = require('popsicle-server');
-var finalhandler = require('finalhandler');
-var Router = require('./');
+require('es6-promise').polyfill()
+
+var methods = require('methods')
+var expect = require('chai').expect
+var popsicle = require('popsicle')
+var server = require('popsicle-server')
+var finalhandler = require('finalhandler')
+var Router = require('./')
 
 describe('Router', function () {
   it('should be a function', function () {
-    expect(Router).to.be.a('function');
-  });
+    expect(Router).to.be.a('function')
+  })
 
   it('should return a function', function () {
-    expect(new Router()).to.be.a('function');
-  });
+    expect(new Router()).to.be.a('function')
+  })
 
   it('should reject missing callback', function () {
-    var router = new Router();
+    var router = new Router()
 
     expect(router.bind(router, {}, {}))
-      .to.throw(/argument callback is required/);
-  });
+      .to.throw(/argument callback is required/)
+  })
 
   it('should not crash with invalid decode', function () {
-    var router = new Router();
+    var router = new Router()
 
     router.get('/{id}', { id: { type: 'string' } }, function (req, res, next) {
-      return next();
-    }, helloWorld);
+      return next()
+    }, helloWorld)
 
     return popsicle('/foo%d')
       .use(server(createServer(router)))
       .then(function (res) {
-        expect(res.status).to.equal(400);
-      });
-  });
+        expect(res.status).to.equal(400)
+      })
+  })
 
   describe('Router#all(path, fn)', function () {
     it('should be chainable', function () {
-      var router = new Router();
+      var router = new Router()
 
-      expect(router.all('/', helloWorld)).to.equal(router);
-    });
+      expect(router.all('/', helloWorld)).to.equal(router)
+    })
 
     it('should respond to all methods', function () {
-      var router = new Router();
+      var router = new Router()
 
-      router.all('/', helloWorld);
+      router.all('/', helloWorld)
 
       return Promise.all(methods.map(function (method) {
         // This is tricky.
         if (method === 'connect') {
-          return;
+          return
         }
 
         return popsicle({ url: '/', method: method })
           .use(server(createServer(router)))
           .then(function (res) {
-            expect(res.status).to.equal(200);
+            expect(res.status).to.equal(200)
             expect(res.body).to.equal(
               method === 'head' ? null : 'hello, world'
-            );
-          });
-      }));
-    });
+            )
+          })
+      }))
+    })
 
     it('should not stack overflow with many registered routes', function () {
-      var router = new Router();
+      var router = new Router()
 
       for (var i = 0; i < 6000; i++) {
-        router.get('/thing' + i, helloWorld);
+        router.get('/thing' + i, helloWorld)
       }
 
-      router.get('/', helloWorld);
+      router.get('/', helloWorld)
 
       return popsicle('/')
         .use(server(createServer(router)))
         .then(function (res) {
-          expect(res.status).to.equal(200);
-          expect(res.body).to.equal('hello, world');
-        });
-    });
-  });
+          expect(res.status).to.equal(200)
+          expect(res.body).to.equal('hello, world')
+        })
+    })
+  })
 
   describe('methods', function () {
     methods.forEach(function (method) {
       if (method === 'connect') {
-        return;
+        return
       }
 
       it('Router#' + method, function () {
-        var router = new Router();
+        var router = new Router()
 
-        router[method]('/foo', helloWorld);
+        router[method]('/foo', helloWorld)
 
         return popsicle({ url: '/foo', method: method })
           .use(server(createServer(router)))
           .then(function (res) {
-            expect(res.status).to.equal(200);
+            expect(res.status).to.equal(200)
             expect(res.body).to.equal(
               method === 'head' ? null : 'hello, world'
-            );
-          });
-      });
-    });
-  });
+            )
+          })
+      })
+    })
+  })
 
   describe('schemas', function () {
     methods.forEach(function (method) {
       if (method === 'connect') {
-        return;
+        return
       }
 
       it('Router#' + method, function () {
-        var router = new Router();
+        var router = new Router()
 
         router[method]('/{id}', {
           id: {
             type: 'number'
           }
         }, function (req, res) {
-          res.setHeader('x-typeof', typeof req.params.id);
-          res.end();
-        });
+          res.setHeader('x-typeof', typeof req.params.id)
+          res.end()
+        })
 
         return popsicle({ url: '/123', method: method })
           .use(server(createServer(router)))
           .then(function (res) {
-            expect(res.status).to.equal(200);
-            expect(res.get('x-typeof')).to.equal('number');
-          });
-      });
-    });
-  });
+            expect(res.status).to.equal(200)
+            expect(res.get('x-typeof')).to.equal('number')
+          })
+      })
+    })
+  })
 
   describe('Router#use(path, fn)', function () {
     it('should be able to use a middleware function', function () {
-      var router = new Router();
+      var router = new Router()
 
       router.use(function (req, res) {
-        res.setHeader('x-url', req.url);
-        res.end();
-      });
+        res.setHeader('x-url', req.url)
+        res.end()
+      })
 
       return popsicle('/foo')
         .use(server(createServer(router)))
         .then(function (res) {
-          expect(res.status).to.equal(200);
-          expect(res.get('x-url')).to.equal('/foo');
-        });
-    });
+          expect(res.status).to.equal(200)
+          expect(res.get('x-url')).to.equal('/foo')
+        })
+    })
 
     it('should accept a path', function () {
-      var router = new Router();
+      var router = new Router()
 
       router.use('/foo', function (req, res) {
-        res.setHeader('x-url', req.url);
-        res.end();
-      });
+        res.setHeader('x-url', req.url)
+        res.end()
+      })
 
       return popsicle('/foo')
         .use(server(createServer(router)))
         .then(function (res) {
-          expect(res.status).to.equal(200);
-          expect(res.get('x-url')).to.equal('/');
-        });
-    });
+          expect(res.status).to.equal(200)
+          expect(res.get('x-url')).to.equal('/')
+        })
+    })
 
     it('should accept a path and schema', function () {
-      var router = new Router();
+      var router = new Router()
 
       router.use('/{path}', {
         path: {
@@ -180,28 +182,28 @@ describe('Router', function () {
           ]
         }
       }, function (req, res) {
-        res.setHeader('x-url', req.url);
-        res.end();
-      });
+        res.setHeader('x-url', req.url)
+        res.end()
+      })
 
       return popsicle('/bar')
         .use(server(createServer(router)))
         .then(function (res) {
-          expect(res.status).to.equal(200);
-          expect(res.get('x-url')).to.equal('/');
-        });
-    });
-  });
-});
+          expect(res.status).to.equal(200)
+          expect(res.get('x-url')).to.equal('/')
+        })
+    })
+  })
+})
 
 function helloWorld (req, res) {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('hello, world');
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/plain')
+  res.end('hello, world')
 }
 
 function createServer (router) {
   return function (req, res) {
-    router(req, res, finalhandler(req, res));
-  };
+    router(req, res, finalhandler(req, res))
+  }
 }
